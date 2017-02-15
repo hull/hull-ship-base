bc482c55113f288a634462941f92957c837842a9
Romain Dardour <romain@hull.io>
2017-02-15 16:01:23 +0100

    un-nest

--- src/queue/index.js
+++ src/queue/index.js
@@ -1,2 +1,16 @@
-export default from "./queue-agent";
+import queueCreate from "./create";
+import KueAdapter from "./adapter/kue";
 
+export default class QueueAgent {
+
+  constructor(adapterName, options) {
+    this.adapter = new KueAdapter(options);
+    this.middleware = this.middleware.bind(this);
+  }
+
+  middleware(req, res, next) {
+    req.hull = req.hull || {};
+    req.hull.queue = req.hull.queue || queueCreate.bind(null, this.adapter, req);
+    return next();
+  }
+}
